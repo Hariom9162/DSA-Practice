592e6c7e796b476e9bf34b196d03fa3e25021842
Hariom Kumar <hariomkumar17082004@gmail.com>
2026-07-23 14:27:25 +0530

    Add Solution of Find Second Largest and Smallest Element

---
+++ Arrays/CodingNinja/GetSecLarSmalNo.java
@@ -0,0 +1,42 @@
+ /*
+Problem : Second Largest Element
+Source  : Striver A2Z
+Platform: Coding Ninjas
+Difficulty: Easy
+*/
+ import java.util.*;
+ public class GetSecLarSmalNo{
+	public static int[] getSecondOrderElements(int n, int[] a) {
+       int slargest=secondLargest(a,n);
+	   int ssmallest=secondSmallest(a,n);
+	   return new int[] {slargest,ssmallest};
+	}
+	public static int secondLargest(int[] a,int n) {
+       int largest=a[0];
+	   int slargest=-1;
+	   for(int i=1;i<n;i++){
+          if(a[i]>largest){
+			  slargest=largest;
+			  largest=a[i];
+		  }else if(a[i]<largest && a[i]>slargest ){
+             slargest=a[i];
+		  }
+		 
+	    }
+		return slargest;
+	}
+	public static int secondSmallest(int[] a,int n){
+     int smallest=a[0];
+	 int ssmallest=Integer.MAX_VALUE;
+	 for(int i=1;i<n;i++){
+		 if(a[i]<smallest){
+			 ssmallest=smallest;
+			 smallest=a[i];
+		 }else if(a[i]!=smallest && a[i]<ssmallest){
+			 ssmallest=a[i];
+		 }
+	    }
+      return ssmallest;
+	}
+
+}
