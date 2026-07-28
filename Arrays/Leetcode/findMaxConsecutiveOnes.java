c5f0b86fe2311deaacfde11796128319c6973d9f
Hariom Kumar <hariomkumar17082004@gmail.com>
2026-07-28 21:56:00 +0530

    Add findMaxConsecutiveOnes Solution

---
+++ Arrays/Leetcode/findMaxConsecutiveOnes.java
@@ -0,0 +1,20 @@
+// leetcode 485
+//striver 
+class findMaxConsecutiveOnes{
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int maxi=0;
+
+        for(int i=0;i<n;i++){
+              if(nums[i]==1){
+                count++;
+                maxi=Math.max(count,maxi);
+            }else{
+                count=0;
+            }
+
+        }
+        return maxi;
+    }
+}
