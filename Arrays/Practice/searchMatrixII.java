86ca80b031a5569d211dedee9de96b79a65c3ce0
Hariom Kumar <hariomkumar17082004@gmail.com>
2026-08-03 23:33:39 +0530

    searchMatrixII.java

---
+++ Arrays/Practice/searchMatrixII.java
@@ -0,0 +1,10 @@
+public class searchMatrixII{
+     public boolean searchMatrix(int[][] matrix,int target){
+	    int totalRow=matrix.length;
+		
+	 }
+  
+  public static void main(String args[]){
+  
+  }
+}
