2d13864961d88dbf71ecc8e47ac7858f2e04cf56
Hariom Kumar <hariomkumar17082004@gmail.com>
2026-08-29 20:37:00 +0530

    Add this file

---
+++ ArraysCC.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ArraysCC{
+    public static void main(String[]args){
+	  int marks[]=new int[50];
+	  Scanner sc=new Scanner(System.in);
+	  //int numbers[]={1,2,3};
+	  marks[0]=sc.nextInt();//phy
+	  marks[1]=sc.nextInt();//chem
+	  marks[2]=sc.nextInt();//math
+	  
+	  System.out.println("phy : " +marks[0]);
+	  System.out.println("chem : " +marks[1]);
+	  System.out.println("math : " +marks[2]);
+	  
+	  marks[2]=70;
+     System.out.println("math : "+marks[2]);	  
+	  }
+   }
+   
