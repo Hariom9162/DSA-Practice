b9d23dd834c329eff092c8ed7c9ff03831f4f4e4
Hariom Kumar <hariomkumar17082004@gmail.com>
2026-08-30 20:05:48 +0530

    Add this file

---
+++ Smlarr.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Smlarr{
+     public static int getSmallest(int numbers[]){
+	   int smallest=Integer.MAX_VALUE;
+	   for(int i=0;i<numbers.length;i++){
+	   if(smallest>numbers[i]){
+	     smallest=numbers[i];
+	   }
+	   }
+	   return smallest;
+	 }
+  public static void main(String[]args){
+     int numbers[]={1,2,6,3,5};
+	 System.out.println("smallest value is:"+getSmallest(numbers));
+  }
+}
