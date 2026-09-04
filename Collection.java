8e830bd03836f0576b6b25d47c665097c6b59966
Hariom Kumar <hariomkumar17082004@gmail.com>
2026-09-04 21:15:00 +0530

    Add this file

---
+++ Collection.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Collection{
+    public static void main(String []args){
+	      ArrayList<Integer> list=new ArrayList<Integer>();
+		  list.add(10);
+		  list.add(20);
+		  list.add(30);
+		  System.out.println(list);
+	}
+}
